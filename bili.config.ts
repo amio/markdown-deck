a7fff4ea281f597bd38f0e74e334d7030806a814
amio <amio.cn@gmail.com>
2019-09-28 23:19:37 +0800

    chore: rename dist file

--- bili.config.ts
+++ bili.config.ts
@@ -3,13 +3,13 @@ import { Config } from 'bili'
 const config: Config = {
   input: 'src/index.ts',
   output: {
-    fileName: 'index.min.js',
+    fileName: 'markdown-deck.min.js',
     sourceMap: true,
   },
-  bundleNodeModules: true,
   plugins: {
     babel: false
   },
+  bundleNodeModules: true,
 }
 
 export default config
