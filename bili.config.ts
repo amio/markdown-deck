414699ae7959a7d0e28028f64d093f8fd8d10fd1
amio <amio.cn@gmail.com>
2019-11-07 15:14:18 +0800

    chore: remove sourcemap from dist files

--- bili.config.ts
+++ bili.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   input: 'src/index.ts',
   output: {
     fileName: 'markdown-deck.min.js',
-    sourceMap: true,
+    sourceMap: false,
   },
   plugins: {
     babel: false
